a4ab4c06ce9cbd55403e81abe61be688985055ba
William Sharp <wsharp@archlt.com>
2017-11-19 20:21:50 -0500

    moving api configuration to the config files

--- Stargate.Web/config/prod.env.js
+++ Stargate.Web/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_HOST: '"localhost"',
+  API_PORT: 8081
 }
--- Stargate.Web/src/services/http-common.js
+++ Stargate.Web/src/services/http-common.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `http://localhost:54115/api/`
+  baseURL: `http://${process.env.API_HOST}:${process.env.API_PORT}/api/`
 });
